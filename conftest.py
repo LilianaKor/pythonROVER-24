a83e371c92e923e6f5f1b763c5a99a425299b8b0
Liliana <lilianakoric@gmail.com>
2024-07-24 20:56:06 -0700

    add fixture in config.py

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from _pytest.fixtures import SubRequest
 
 
 @pytest.fixture()
@@ -12,3 +13,17 @@ def driver():
     page = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
     yield page
     page.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser')
+
+
+# @pytest.fixture()
+# def something(request: SubRequest):
+#     if request.config.getoption('--browser').upper() == 'FF':
+#         driver = webdriver.Firefox()
+#     elif request.config.getoption('--browser').lower() == 'chrome':
+#         driver = webdriver.Chrome()
+#     yield driver
+#     driver.quit()
